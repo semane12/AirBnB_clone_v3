8f63b7a3e44fb62fcc9e41bc5ee060d95aa998ee
Semane S. Fentaye <114406855+semane12@users.noreply.github.com>
2023-05-29 22:07:54 +0300

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,12 +8,12 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def status():
     """
     Returns a JSON status
     """
-    return jsonify({"status": "OK"})
+    return jsonify({"stats": "OK"})
 
 
 @app_views.route('/stats', strict_slashes=False)
