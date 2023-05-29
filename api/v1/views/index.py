f857ea45142a20e0531614122d1324834569bb74
Semane S. Fentaye <114406855+semane12@users.noreply.github.com>
2023-05-29 22:12:20 +0300

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-This module contains endpoint(route) status
+This module contains endpoint(route) stats
 """
 from models import storage
 from flask import Flask
 from api.v1.views import app_views
 from flask import jsonify
 
-
 @app_views.route('/stats', strict_slashes=False)
-def status():
+def stats():
     """
-    Returns a JSON status
+    Returns a JSON stats
     """
     return jsonify({"stats": "OK"})
-
 
 @app_views.route('/stats', strict_slashes=False)
 def count():
